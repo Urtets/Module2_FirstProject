73b8174335e67ea5f09c66987776502df0e7d540
Юрий <urtets@gmail.com>
2024-09-02 12:18:07 +0300

    Second Commit

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,11 +1,10 @@
-def find_num():
-    for k in range(3, 21):
-        result = str(k) + " - "
-        for i in range(1, k):
-            for j in range(i + 1, k):
-                if k % (i + j) == 0:
-                    result = result + str(i) + str(k)
+def find_num(n):
+    result = ''
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            if n % (i + j) == 0:
+                result += str(i) + str(j)
+    return f'{n} - {result}'
 
-        print(result)
-
-find_num()
+for k in range(3, 20):
+    print(find_num(k))
