f798d47c9a87b6b613f4b943c2a0b1ea8ea112d1
Юрий <urtets@gmail.com>
2024-09-02 11:53:54 +0300

    Second Commit

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,20 +1,11 @@
-import random
+def find_num():
+    for k in range(3, 21):
+        result = str(k) + " - "
+        for i in range(1, k):
+            for j in range(i + 1, k):
+                if k % (i + j) == 0:
+                    result = result + str(i) + str(k)
 
-random_int = random.randint(3, 20)
-num_1 = 4
-print(num_1)
-def find_num2(a):
-    b = ""
-    for i in range(1, a + 1):
-        if i == a:
-            b = b + str(i)
-        for j in range(1, a + 1):
-            print(f'Первое число {i}, второе число {j}')
-            c = i + j
-            print(f'Число {c}')
-            if a % c == 0 and j < i:
-                b = b + str(i) + "+" + str(j) + " "
+        print(result)
 
-    return b
-
-print(find_num2(num_1))
+find_num()
